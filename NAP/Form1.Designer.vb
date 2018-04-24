e6f42eebfa399699dfc7465bf59211110091af98
Jon Martin <jonmartin721@gmail.com>
2018-04-23 22:22:26 -0500

    Add project files.

---
+++ NAP/Form1.Designer.vb
@@ -0,0 +1,437 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_One = New System.Windows.Forms.Label()
+        Me.lbl_Two = New System.Windows.Forms.Label()
+        Me.lbl_Three = New System.Windows.Forms.Label()
+        Me.lbl_Four = New System.Windows.Forms.Label()
+        Me.lbl_Five = New System.Windows.Forms.Label()
+        Me.lbl_Six = New System.Windows.Forms.Label()
+        Me.lbl_Seven = New System.Windows.Forms.Label()
+        Me.lbl_Eight = New System.Windows.Forms.Label()
+        Me.lbl_Nine = New System.Windows.Forms.Label()
+        Me.lbl_Ten = New System.Windows.Forms.Label()
+        Me.txt_One = New System.Windows.Forms.TextBox()
+        Me.txt_Two = New System.Windows.Forms.TextBox()
+        Me.txt_Three = New System.Windows.Forms.TextBox()
+        Me.txt_Four = New System.Windows.Forms.TextBox()
+        Me.txt_Five = New System.Windows.Forms.TextBox()
+        Me.txt_Six = New System.Windows.Forms.TextBox()
+        Me.txt_Seven = New System.Windows.Forms.TextBox()
+        Me.txt_Eight = New System.Windows.Forms.TextBox()
+        Me.txt_Nine = New System.Windows.Forms.TextBox()
+        Me.txt_Ten = New System.Windows.Forms.TextBox()
+        Me.bttn_Analyze = New System.Windows.Forms.Button()
+        Me.bttn_Exit = New System.Windows.Forms.Button()
+        Me.bttn_Clear = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lbl_lowest = New System.Windows.Forms.Label()
+        Me.lbl_Highest = New System.Windows.Forms.Label()
+        Me.lbl_Total = New System.Windows.Forms.Label()
+        Me.lbl_Average = New System.Windows.Forms.Label()
+        Me.lbl_LowestAnalyze = New System.Windows.Forms.Label()
+        Me.lbl_HighestAnalyze = New System.Windows.Forms.Label()
+        Me.lbl_TotalAnalyze = New System.Windows.Forms.Label()
+        Me.lbl_AverageAnalyze = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lbl_One
+        '
+        Me.lbl_One.AutoSize = True
+        Me.lbl_One.Location = New System.Drawing.Point(34, 45)
+        Me.lbl_One.Name = "lbl_One"
+        Me.lbl_One.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_One.TabIndex = 0
+        Me.lbl_One.Text = "Number 1:"
+        '
+        'lbl_Two
+        '
+        Me.lbl_Two.AutoSize = True
+        Me.lbl_Two.Location = New System.Drawing.Point(34, 90)
+        Me.lbl_Two.Name = "lbl_Two"
+        Me.lbl_Two.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Two.TabIndex = 0
+        Me.lbl_Two.Text = "Number 2:"
+        '
+        'lbl_Three
+        '
+        Me.lbl_Three.AutoSize = True
+        Me.lbl_Three.Location = New System.Drawing.Point(34, 135)
+        Me.lbl_Three.Name = "lbl_Three"
+        Me.lbl_Three.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Three.TabIndex = 0
+        Me.lbl_Three.Text = "Number 3:"
+        '
+        'lbl_Four
+        '
+        Me.lbl_Four.AutoSize = True
+        Me.lbl_Four.Location = New System.Drawing.Point(34, 180)
+        Me.lbl_Four.Name = "lbl_Four"
+        Me.lbl_Four.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Four.TabIndex = 0
+        Me.lbl_Four.Text = "Number 4:"
+        '
+        'lbl_Five
+        '
+        Me.lbl_Five.AutoSize = True
+        Me.lbl_Five.Location = New System.Drawing.Point(34, 225)
+        Me.lbl_Five.Name = "lbl_Five"
+        Me.lbl_Five.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Five.TabIndex = 0
+        Me.lbl_Five.Text = "Number 5:"
+        '
+        'lbl_Six
+        '
+        Me.lbl_Six.AutoSize = True
+        Me.lbl_Six.Location = New System.Drawing.Point(34, 270)
+        Me.lbl_Six.Name = "lbl_Six"
+        Me.lbl_Six.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Six.TabIndex = 0
+        Me.lbl_Six.Text = "Number 6:"
+        '
+        'lbl_Seven
+        '
+        Me.lbl_Seven.AutoSize = True
+        Me.lbl_Seven.Location = New System.Drawing.Point(34, 315)
+        Me.lbl_Seven.Name = "lbl_Seven"
+        Me.lbl_Seven.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Seven.TabIndex = 0
+        Me.lbl_Seven.Text = "Number 7:"
+        '
+        'lbl_Eight
+        '
+        Me.lbl_Eight.AutoSize = True
+        Me.lbl_Eight.Location = New System.Drawing.Point(34, 360)
+        Me.lbl_Eight.Name = "lbl_Eight"
+        Me.lbl_Eight.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Eight.TabIndex = 0
+        Me.lbl_Eight.Text = "Number 8:"
+        '
+        'lbl_Nine
+        '
+        Me.lbl_Nine.AutoSize = True
+        Me.lbl_Nine.Location = New System.Drawing.Point(34, 405)
+        Me.lbl_Nine.Name = "lbl_Nine"
+        Me.lbl_Nine.Size = New System.Drawing.Size(82, 20)
+        Me.lbl_Nine.TabIndex = 0
+        Me.lbl_Nine.Text = "Number 9:"
+        '
+        'lbl_Ten
+        '
+        Me.lbl_Ten.AutoSize = True
+        Me.lbl_Ten.Location = New System.Drawing.Point(34, 450)
+        Me.lbl_Ten.Name = "lbl_Ten"
+        Me.lbl_Ten.Size = New System.Drawing.Size(91, 20)
+        Me.lbl_Ten.TabIndex = 0
+        Me.lbl_Ten.Text = "Number 10:"
+        '
+        'txt_One
+        '
+        Me.txt_One.Location = New System.Drawing.Point(140, 39)
+        Me.txt_One.Name = "txt_One"
+        Me.txt_One.Size = New System.Drawing.Size(100, 26)
+        Me.txt_One.TabIndex = 1
+        Me.txt_One.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Two
+        '
+        Me.txt_Two.Location = New System.Drawing.Point(140, 84)
+        Me.txt_Two.Name = "txt_Two"
+        Me.txt_Two.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Two.TabIndex = 2
+        Me.txt_Two.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Three
+        '
+        Me.txt_Three.Location = New System.Drawing.Point(140, 129)
+        Me.txt_Three.Name = "txt_Three"
+        Me.txt_Three.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Three.TabIndex = 3
+        Me.txt_Three.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Four
+        '
+        Me.txt_Four.Location = New System.Drawing.Point(140, 174)
+        Me.txt_Four.Name = "txt_Four"
+        Me.txt_Four.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Four.TabIndex = 4
+        Me.txt_Four.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Five
+        '
+        Me.txt_Five.Location = New System.Drawing.Point(140, 219)
+        Me.txt_Five.Name = "txt_Five"
+        Me.txt_Five.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Five.TabIndex = 5
+        Me.txt_Five.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Six
+        '
+        Me.txt_Six.Location = New System.Drawing.Point(140, 264)
+        Me.txt_Six.Name = "txt_Six"
+        Me.txt_Six.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Six.TabIndex = 6
+        Me.txt_Six.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Seven
+        '
+        Me.txt_Seven.Location = New System.Drawing.Point(140, 309)
+        Me.txt_Seven.Name = "txt_Seven"
+        Me.txt_Seven.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Seven.TabIndex = 7
+        Me.txt_Seven.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Eight
+        '
+        Me.txt_Eight.Location = New System.Drawing.Point(140, 354)
+        Me.txt_Eight.Name = "txt_Eight"
+        Me.txt_Eight.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Eight.TabIndex = 8
+        Me.txt_Eight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Nine
+        '
+        Me.txt_Nine.Location = New System.Drawing.Point(140, 399)
+        Me.txt_Nine.Name = "txt_Nine"
+        Me.txt_Nine.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Nine.TabIndex = 9
+        Me.txt_Nine.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_Ten
+        '
+        Me.txt_Ten.Location = New System.Drawing.Point(140, 444)
+        Me.txt_Ten.Name = "txt_Ten"
+        Me.txt_Ten.Size = New System.Drawing.Size(100, 26)
+        Me.txt_Ten.TabIndex = 10
+        Me.txt_Ten.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'bttn_Analyze
+        '
+        Me.bttn_Analyze.Location = New System.Drawing.Point(65, 530)
+        Me.bttn_Analyze.Name = "bttn_Analyze"
+        Me.bttn_Analyze.Size = New System.Drawing.Size(150, 52)
+        Me.bttn_Analyze.TabIndex = 2
+        Me.bttn_Analyze.Text = "Analyze"
+        Me.bttn_Analyze.UseVisualStyleBackColor = True
+        '
+        'bttn_Exit
+        '
+        Me.bttn_Exit.Location = New System.Drawing.Point(429, 530)
+        Me.bttn_Exit.Name = "bttn_Exit"
+        Me.bttn_Exit.Size = New System.Drawing.Size(150, 52)
+        Me.bttn_Exit.TabIndex = 4
+        Me.bttn_Exit.Text = "Exit"
+        Me.bttn_Exit.UseVisualStyleBackColor = True
+        '
+        'bttn_Clear
+        '
+        Me.bttn_Clear.Location = New System.Drawing.Point(247, 530)
+        Me.bttn_Clear.Name = "bttn_Clear"
+        Me.bttn_Clear.Size = New System.Drawing.Size(150, 52)
+        Me.bttn_Clear.TabIndex = 3
+        Me.bttn_Clear.Text = "Clear"
+        Me.bttn_Clear.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lbl_One)
+        Me.GroupBox1.Controls.Add(Me.lbl_Two)
+        Me.GroupBox1.Controls.Add(Me.lbl_Three)
+        Me.GroupBox1.Controls.Add(Me.lbl_Four)
+        Me.GroupBox1.Controls.Add(Me.txt_Ten)
+        Me.GroupBox1.Controls.Add(Me.lbl_Five)
+        Me.GroupBox1.Controls.Add(Me.txt_Nine)
+        Me.GroupBox1.Controls.Add(Me.lbl_Six)
+        Me.GroupBox1.Controls.Add(Me.txt_Eight)
+        Me.GroupBox1.Controls.Add(Me.lbl_Seven)
+        Me.GroupBox1.Controls.Add(Me.txt_Seven)
+        Me.GroupBox1.Controls.Add(Me.lbl_Eight)
+        Me.GroupBox1.Controls.Add(Me.txt_Six)
+        Me.GroupBox1.Controls.Add(Me.lbl_Nine)
+        Me.GroupBox1.Controls.Add(Me.txt_Five)
+        Me.GroupBox1.Controls.Add(Me.lbl_Ten)
+        Me.GroupBox1.Controls.Add(Me.txt_Four)
+        Me.GroupBox1.Controls.Add(Me.txt_One)
+        Me.GroupBox1.Controls.Add(Me.txt_Three)
+        Me.GroupBox1.Controls.Add(Me.txt_Two)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(280, 492)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Array Numbers"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lbl_AverageAnalyze)
+        Me.GroupBox2.Controls.Add(Me.lbl_TotalAnalyze)
+        Me.GroupBox2.Controls.Add(Me.lbl_HighestAnalyze)
+        Me.GroupBox2.Controls.Add(Me.lbl_LowestAnalyze)
+        Me.GroupBox2.Controls.Add(Me.lbl_Average)
+        Me.GroupBox2.Controls.Add(Me.lbl_Total)
+        Me.GroupBox2.Controls.Add(Me.lbl_Highest)
+        Me.GroupBox2.Controls.Add(Me.lbl_lowest)
+        Me.GroupBox2.Location = New System.Drawing.Point(343, 57)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(289, 391)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Analysis"
+        '
+        'lbl_lowest
+        '
+        Me.lbl_lowest.AutoSize = True
+        Me.lbl_lowest.Location = New System.Drawing.Point(44, 48)
+        Me.lbl_lowest.Name = "lbl_lowest"
+        Me.lbl_lowest.Size = New System.Drawing.Size(182, 20)
+        Me.lbl_lowest.TabIndex = 0
+        Me.lbl_lowest.Text = "Lowerst number in array:"
+        '
+        'lbl_Highest
+        '
+        Me.lbl_Highest.AutoSize = True
+        Me.lbl_Highest.Location = New System.Drawing.Point(44, 132)
+        Me.lbl_Highest.Name = "lbl_Highest"
+        Me.lbl_Highest.Size = New System.Drawing.Size(181, 20)
+        Me.lbl_Highest.TabIndex = 0
+        Me.lbl_Highest.Text = "Highest number in array:"
+        '
+        'lbl_Total
+        '
+        Me.lbl_Total.AutoSize = True
+        Me.lbl_Total.Location = New System.Drawing.Point(44, 216)
+        Me.lbl_Total.Name = "lbl_Total"
+        Me.lbl_Total.Size = New System.Drawing.Size(214, 20)
+        Me.lbl_Total.TabIndex = 0
+        Me.lbl_Total.Text = "Total of the numbers in array:"
+        '
+        'lbl_Average
+        '
+        Me.lbl_Average.AutoSize = True
+        Me.lbl_Average.Location = New System.Drawing.Point(44, 300)
+        Me.lbl_Average.Name = "lbl_Average"
+        Me.lbl_Average.Size = New System.Drawing.Size(214, 20)
+        Me.lbl_Average.TabIndex = 0
+        Me.lbl_Average.Text = "Average number of the array:"
+        '
+        'lbl_LowestAnalyze
+        '
+        Me.lbl_LowestAnalyze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl_LowestAnalyze.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_LowestAnalyze.Location = New System.Drawing.Point(44, 82)
+        Me.lbl_LowestAnalyze.Name = "lbl_LowestAnalyze"
+        Me.lbl_LowestAnalyze.Size = New System.Drawing.Size(181, 28)
+        Me.lbl_LowestAnalyze.TabIndex = 1
+        Me.lbl_LowestAnalyze.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_HighestAnalyze
+        '
+        Me.lbl_HighestAnalyze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl_HighestAnalyze.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_HighestAnalyze.Location = New System.Drawing.Point(44, 172)
+        Me.lbl_HighestAnalyze.Name = "lbl_HighestAnalyze"
+        Me.lbl_HighestAnalyze.Size = New System.Drawing.Size(181, 28)
+        Me.lbl_HighestAnalyze.TabIndex = 1
+        Me.lbl_HighestAnalyze.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_TotalAnalyze
+        '
+        Me.lbl_TotalAnalyze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl_TotalAnalyze.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_TotalAnalyze.Location = New System.Drawing.Point(44, 252)
+        Me.lbl_TotalAnalyze.Name = "lbl_TotalAnalyze"
+        Me.lbl_TotalAnalyze.Size = New System.Drawing.Size(181, 28)
+        Me.lbl_TotalAnalyze.TabIndex = 1
+        Me.lbl_TotalAnalyze.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_AverageAnalyze
+        '
+        Me.lbl_AverageAnalyze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl_AverageAnalyze.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_AverageAnalyze.Location = New System.Drawing.Point(44, 336)
+        Me.lbl_AverageAnalyze.Name = "lbl_AverageAnalyze"
+        Me.lbl_AverageAnalyze.Size = New System.Drawing.Size(181, 28)
+        Me.lbl_AverageAnalyze.TabIndex = 1
+        Me.lbl_AverageAnalyze.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(144.0!, 144.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(644, 588)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.bttn_Clear)
+        Me.Controls.Add(Me.bttn_Exit)
+        Me.Controls.Add(Me.bttn_Analyze)
+        Me.MaximumSize = New System.Drawing.Size(666, 644)
+        Me.MinimumSize = New System.Drawing.Size(666, 644)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Number Analysis Program"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lbl_One As Label
+    Friend WithEvents lbl_Two As Label
+    Friend WithEvents lbl_Three As Label
+    Friend WithEvents lbl_Four As Label
+    Friend WithEvents lbl_Five As Label
+    Friend WithEvents lbl_Six As Label
+    Friend WithEvents lbl_Seven As Label
+    Friend WithEvents lbl_Eight As Label
+    Friend WithEvents lbl_Nine As Label
+    Friend WithEvents lbl_Ten As Label
+    Friend WithEvents txt_One As TextBox
+    Friend WithEvents txt_Two As TextBox
+    Friend WithEvents txt_Three As TextBox
+    Friend WithEvents txt_Four As TextBox
+    Friend WithEvents txt_Five As TextBox
+    Friend WithEvents txt_Six As TextBox
+    Friend WithEvents txt_Seven As TextBox
+    Friend WithEvents txt_Eight As TextBox
+    Friend WithEvents txt_Nine As TextBox
+    Friend WithEvents txt_Ten As TextBox
+    Friend WithEvents bttn_Analyze As Button
+    Friend WithEvents bttn_Exit As Button
+    Friend WithEvents bttn_Clear As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lbl_Highest As Label
+    Friend WithEvents lbl_lowest As Label
+    Friend WithEvents lbl_Average As Label
+    Friend WithEvents lbl_Total As Label
+    Friend WithEvents lbl_AverageAnalyze As Label
+    Friend WithEvents lbl_TotalAnalyze As Label
+    Friend WithEvents lbl_HighestAnalyze As Label
+    Friend WithEvents lbl_LowestAnalyze As Label
+End Class
---
+++ NAP/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub bttn_Analyze_Click(sender As Object, e As EventArgs) Handles bttn_Analyze.Click
+        Dim arrayNumbers() As Double = New Double(9) {CDbl(txt_One.Text), CDbl(txt_Two.Text), CDbl(txt_Three.Text), CDbl(txt_Four.Text), CDbl(txt_Five.Text), CDbl(txt_Six.Text), CDbl(txt_Seven.Text), CDbl(txt_Eight.Text), CDbl(txt_Nine.Text), CDbl(txt_Ten.Text)}
+
+        Dim min As Double = Double.MaxValue
+        Dim max As Double = Double.MinValue
+        Dim sum As Double
+        Dim average As Double
+
+        'Find Lowest and Highest value
+        For x As Integer = 0 To 9
+            'if lower than current min
+            If arrayNumbers(x) < min Then min = arrayNumbers(x)
+            'if higher than current max
+            If arrayNumbers(x) > max Then max = arrayNumbers(x)
+            'add number to running total
+            sum += arrayNumbers(x)
+        Next
+        'calculate the average
+        average = sum / 10
+
+        lbl_AverageAnalyze.Text = average.ToString
+
+        lbl_HighestAnalyze.Text = max.ToString
+
+        lbl_LowestAnalyze.Text = min.ToString
+
+        lbl_TotalAnalyze.Text = sum.ToString
+
+
+
+    End Sub
+
+    Private Sub bttn_Clear_Click(sender As Object, e As EventArgs) Handles bttn_Clear.Click
+        txt_One.Text = String.Empty
+        txt_Two.Text = String.Empty
+        txt_Three.Text = String.Empty
+        txt_Four.Text = String.Empty
+        txt_Five.Text = String.Empty
+        txt_Six.Text = String.Empty
+        txt_Seven.Text = String.Empty
+        txt_Eight.Text = String.Empty
+        txt_Nine.Text = String.Empty
+        txt_Ten.Text = String.Empty
+        lbl_LowestAnalyze.Text = String.Empty
+        lbl_HighestAnalyze.Text = String.Empty
+        lbl_TotalAnalyze.Text = String.Empty
+        lbl_AverageAnalyze.Text = String.Empty
+        txt_One.Focus()
+
+
+    End Sub
+
+    Private Sub bttn_Exit_Click(sender As Object, e As EventArgs) Handles bttn_Exit.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub lbl_Highest_Click(sender As Object, e As EventArgs) Handles lbl_Highest.Click, lbl_Total.Click, lbl_Average.Click
+
+    End Sub
+End Class
